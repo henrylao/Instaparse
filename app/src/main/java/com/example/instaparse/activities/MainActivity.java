ef58337bcfa8833ab42db1945c87493637762578
Henry Lao <hclao1995@gmail.com>
2021-03-09 10:38:41 -0800

    Refactor activity to utilize Fragments

--- app/src/main/java/com/example/instaparse/activities/MainActivity.java
+++ app/src/main/java/com/example/instaparse/activities/MainActivity.java
@@ -3,64 +3,28 @@ package com.example.instaparse.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.instaparse.R;
-import com.example.instaparse.models.Post;
+import com.example.instaparse.fragments.ComposeFragment;
+import com.example.instaparse.fragments.HomeFragment;
+import com.example.instaparse.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
-
-import java.io.File;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
-//    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
-//    public static final int RESULT_OK = 0;
-//    private EditText etDescription;
-//    private Button btnCaptureImage;
-//    private ImageView ivPostImage;
-//    private Button btnSubmit;
-//    private File photoFile;
-//    private String photoFileName = "photo.jpg";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        etDescription = findViewById(R.id.etDescription);
-//        btnCaptureImage = findViewById(R.id.btnCaptureImage);
-//        ivPostImage = findViewById(R.id.ivPostImage);
-//        btnSubmit = findViewById(R.id.btnSubmit);
-//
-//        btnSubmit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String description = etDescription.getText().toString();
-//                if (description.isEmpty()){
-//                    Toast.makeText(MainActivity.this, "Description cannot be empty", Toast.LENGTH_SHORT).show();
-//                    return;
-//                }
-//                ParseUser currentUser = ParseUser.getCurrentUser();
-//                savePost(description, currentUser);
-//            }
-//        });
-
 
         bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -70,60 +34,28 @@ public class MainActivity extends AppCompatActivity {
                 switch (btmNavMenuItem.getItemId()) {
                     case R.id.action_compose:
                         Toast.makeText(MainActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
-//                        fragment = fragment1;
+                        fragment = new ComposeFragment();
                         break;
                     case R.id.action_home:
                         Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
-
-//                        fragment = fragment2;
+                        fragment = new HomeFragment();
                         break;
                     case R.id.action_profile:
                         Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                        fragment = new ProfileFragment();
                         break;
                     default:
-//                        fragment = fragment3;
+                        fragment = new HomeFragment();
                         break;
                 }
-//                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
 //                return false;
             }
         });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
 //        queryPosts();   // get all insta posts pertaining to the user
     }
 
-//    private void savePost(String description, ParseUser user){
-//        Post p = new Post();
-//        p.setDescription(description);
-//        p.setUser(user);
-//        p.saveInBackground(new SaveCallback() {
-//            @Override
-//            public void done(ParseException e) {
-//                if (e != null){
-//                    Log.e(TAG, "Error while saving post!", e);
-//                }
-//                Log.i(TAG, "Post was saved successfully!");
-//                Toast.makeText(MainActivity.this, "Posted!", Toast.LENGTH_SHORT).show();
-//                etDescription.setText(""); // reset text
-//            }
-//        });
-//
-//    }
-//
-//    private void queryPosts() {
-//        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-//        query.include(Post.KEY_USER);
-//        query.findInBackground(new FindCallback<Post>() {
-//            @Override
-//            public void done(List<Post> posts, ParseException e) {
-//                if (e != null) {
-//                    Log.e(TAG, "Issue with getting posts", e);
-//                    return;
-//                }
-//                for (Post p : posts) {
-//                    Log.i(TAG,"Post: " + p.getDescription() + ", username: " + p.getUser().getUsername());
-//                }
-//            }
-//        });
-//    }
 }
