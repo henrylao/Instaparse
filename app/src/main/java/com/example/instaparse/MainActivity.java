cdbf333c1a32984454daeb2afaccb6ded409f1ab
Henry Lao <hclao1995@gmail.com>
2021-03-09 06:41:58 -0800

    Add completion of Ep 7: usage of fragments

--- app/src/main/java/com/example/instaparse/MainActivity.java
+++ app/src/main/java/com/example/instaparse/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.instaparse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.instaparse.model.Post;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
@@ -19,8 +23,6 @@ import com.parse.SaveCallback;
 
 import java.util.List;
 
-import javax.microedition.khronos.egl.EGLDisplay;
-
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnCaptureImage;
     private ImageView ivPostImage;
     private Button btnSubmit;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         btnCaptureImage = findViewById(R.id.btnCaptureImage);
         ivPostImage = findViewById(R.id.ivPostImage);
         btnSubmit = findViewById(R.id.btnSubmit);
-
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +53,33 @@ public class MainActivity extends AppCompatActivity {
                 }
                 ParseUser currentUser = ParseUser.getCurrentUser();
                 savePost(description, currentUser);
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem btmNavMenuItem) {   // item is one of the elements in the bottom nav view
+                Fragment fragment;
+                switch (btmNavMenuItem.getItemId()) {
+                    case R.id.action_compose:
+                        Toast.makeText(MainActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
+//                        fragment = fragment1;
+                        break;
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+
+//                        fragment = fragment2;
+                        break;
+                    case R.id.action_profile:
+                        Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+//                        fragment = fragment3;
+                        break;
+                }
+//                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+//                return false;
             }
         });
 //        queryPosts();   // get all insta posts pertaining to the user
